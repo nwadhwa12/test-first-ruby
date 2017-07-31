7f5e725303cbfe955eaf07bcb5b896edc3706213
Nitin Wadhwani <nitin101262cse@gmail.com>
2017-07-31 19:27:50 -0400

    Update 00_hello.rb

---
+++ lib/00_hello.rb
@@ -0,0 +1,9 @@
+
+     static void hello()
+     {
+         System.out.println("Hello");
+     }
+     static void greet(String name)
+     {
+         System.out.println("Hello, " + name);
+     }
