2f285bba1cd5d6df0834ce9250a5904834ecab77
Nitin Wadhwani <nitin101262cse@gmail.com>
2017-08-11 16:33:07 -0700

    Update 11_dictionary.rb

--- lib/11_dictionary.rb
+++ lib/11_dictionary.rb
@@ -1,3 +1,52 @@
 class Dictionary
-  # TODO: your code goes here!
+	attr_accessor :ent
+	
+def initialize
+	@ent = {}
+end
+	
+def add(number)
+	if number.is_a?(String)
+	@ent[number]=nil
+	else
+	number.each do |key,value|
+	@ent[number]= value
+	end
+  end
+end
+	
+	
+	def keywords
+		@ent.keys.sort
+	end
+
+	def include?(number)
+		if @ent.keys.include?(number)
+		return	true
+		else 
+		return	false
+		end
+		
+	end
+	
+	def find(number)
+		my_ans={}
+		@ent.each do |x,y|
+		if key[0...number.length] == number
+		my_ans[x]=y
+		end
+		if number[0..1] == x[0..1]
+		my_ans[x]=y
+		end
+		end
+		return answer
+	end
+	
+	
+	def printable
+	arr=@ent.sort.map do |key,value|
+	"[#{key}] \"#{value}\""
+	end
+	arr.join("\n")
+	end
 end
