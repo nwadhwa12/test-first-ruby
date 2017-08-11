6fb54d036106ddf6449eec04b410a202adbd8a0c
Nitin Wadhwani <nitin101262cse@gmail.com>
2017-08-11 16:31:29 -0700

    Update 08_book_titles.rb

--- lib/08_book_titles.rb
+++ lib/08_book_titles.rb
@@ -1,3 +1,18 @@
 class Book
-  # TODO: your code goes here!
+  attr_accessor :title
+  def title(word)
+    nums = Array["the","a","an","and","in","of"]
+    words = word.split(" ")
+    words[0].capitalize!
+    words.each do |x|
+      if nums.include? x
+        
+      else
+        x.capitalize!
+      end
+    
+    end
+  @title = words.join(" ")
+  end
+  
 end
