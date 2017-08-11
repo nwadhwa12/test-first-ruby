99de98d4292bbe0a8e7254a701056f138db86dc5
Nitin Wadhwani <nitin101262cse@gmail.com>
2017-08-11 16:33:33 -0700

    Update 12_rpn_calculator.rb

--- lib/12_rpn_calculator.rb
+++ lib/12_rpn_calculator.rb
@@ -1,3 +1,35 @@
 class RPNCalculator
-  # TODO: your code goes here!
+
+attr_accessor :calculator
+
+def initialize()
+@calculator=[]
+end
+	
+def push(x)
+@calculator << x
+end
+	
+def value
+@calculator.last
+end
+	
+def add
+if @calculator.size>=2
+sum_num=@calculator.pop + @calculator.pop
+@calculator << sum_num
+else
+raise "calculator is empty"
+end
+end
+
+def subtract
+if @calculator.size>=2
+second_num=@calculator.pop
+first_num=@calculator.pop
+difference=first_num - second_num
+@calculator << difference
+else
+raise "calculator is empty"
+end
 end
